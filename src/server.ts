741d2e995f94725c67905641ddd104a7c3233551
Leandro Fonseca <leandromkt@outlook.com>
2025-07-20 14:38:16 -0300

    filter driver

--- src/server.ts
+++ src/server.ts
@@ -33,6 +33,26 @@ server.get("/drivers", async (request, response) => {
     return { drivers };
 });
 
+interface DriverParams {
+    id: string;
+}
+
+server.get<{ Params: DriverParams }>(
+    "/drivers/:id",
+    async (request, response) => {
+      const id = parseInt(request.params.id);
+      const driver = drivers.find((d) => d.id === id);
+  
+      if (!driver) {
+        response.type("application/json").code(404);
+        return { message: "Driver Not Found" };
+      } else {
+        response.type("application/json").code(200);
+        return { driver };
+      }
+    }
+);
+
 server.listen({ port: 3333 }, () => {
     console.log("Server init");
 });
